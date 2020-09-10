3fa6760ca68c6b644acec2bcee1f28dc8deed6b4
Khadijasam <70614192+Khadijasam@users.noreply.github.com>
2020-09-10 16:19:00 +0530

    Add files via upload

---
+++ PrimeNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class PrimeNum
+    {
+        public static void CheckPrime(int n)
+        {
+            int i,m = 0,flag=0;
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    Console.Read();
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.Write("Number is Prime.");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Enter the Number to check Prime: ");
+        //    int n = int.Parse(Console.ReadLine());
+        //    PrimeNum.CheckPrime(n);
+        //}
+
+        static void Main(String[] args)
+        {
+            string sname="", scrs="";
+            int smrks=0, srno=0;
+            
+            Student.GetData();
+            Student.ShowData(sname, scrs, srno, smrks);
+            Console.ReadLine();
+            char grade = Student.CalGrade(smrks);
+            Console.Read();
+            //Student.ShowData(sname, scrs, srno, smrks, grade);
+            
+        }
+    }
+}
